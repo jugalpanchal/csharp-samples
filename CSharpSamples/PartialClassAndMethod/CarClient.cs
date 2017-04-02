6a00ab71ad656e1e47bac6a1048c07ed3b58e3cc
Jugal Panchal <jugalpanchal@gmail.com>
2017-04-03 02:23:47 +0530

    Added program for parital class and method

---
+++ CSharpSamples/PartialClassAndMethod/CarClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialClassAndMethod
+{
+    public partial class Car
+    {
+        partial void OnSomethingHappened(String str)
+        {
+            Console.WriteLine("Something happened: {0}", str);
+        }
+    }
+}
---
+++ CSharpSamples/PartialClassAndMethod/CarServer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialClassAndMethod
+{
+    public partial class Car
+    {
+        partial void OnSomethingHappened(string str);
+    }
+}
