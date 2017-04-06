b53bc061d0e5d27a83bcb4c26f1520c663bfd281
Jugal Panchal <jugalpanchal@gmail.com>
2017-04-06 07:09:03 +0530

    Out and Ref

---
+++ CSharpSamples/OutAndRef/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutAndRef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 01;
+            Program p = new Program();
+            //p.Convert(ref value);
+            p.Convert(out value);
+
+        }
+
+        void Convert(out int value)
+        {
+            value = 20;/* Must be initialize */
+        }
+        /*
+        void Convert(ref int value)
+        {
+            //value = 20;// Option
+        }
+        */
+    }
+}
